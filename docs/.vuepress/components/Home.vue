0799b1cef8faa1f9119ad1dbe5d296feb933dba7
Kurtil <gaetan.lagier@hotmail.fr>
2021-06-03 10:25:06 +0200

    remove 2d-engine to make the app build -_-

--- docs/.vuepress/components/Home.vue
+++ docs/.vuepress/components/Home.vue
@@ -13,91 +13,18 @@
       Drawn shapes are dynamics and can be edited on the fly.
     </div>
     <img :src="$withBase('/assets/img/plugin.measures.gif')">
-    <div>
-      <b>Try it !</b>
-    </div>
-    <div>
-      Mouse move on the following canvas:
-    </div>
-    <canvas id="canvas" style="background-color: #f4f4f4"></canvas>
   </div>
 </template>
 
-<script>
-import { onMounted } from "vue"
 
-import makeViewer from "@bimdata/2d-engine"
-
-export default {
-  setup() {
-    function addObjects(canvas) {
-            /**
-       * @type { E2D.Viewer }
-       */
-      const viewer = makeViewer({ canvas })
-
-      const arc = viewer.scene.addObject({
-        textureTint: 0xFFFF00,
-        texture: "solid",
-        textureOpacity: 0.3,
-        lineWidth: 4,
-        geometry: [
-          {
-            type: "arc",
-            x: 200,
-            y: 200,
-            radius: 200
-          }
-        ]
-      });
-
-      const line = viewer.scene.addObject({
-        lineColor: 0x000000,
-        lineWidth: 4,
-        lineDash: [5, 10, 15],
-        geometry: [
-          [
-                200, 200,
-                200, 200
-          ],
-        ]
-      });
-
-      viewer.camera.fitView({minX: 0, minY: 0, maxX: 400, minY: 400})
-      viewer.ui.disconnect()
-      canvas.addEventListener("mousemove", e => {
-        // console.log("mousemove")
-        const point = line.geometry.getShape().points[1]
-        const circle = arc.geometry.getShape()
-
-        const canvasPosX = e.offsetX / window.devicePixelRatio
-        const canvasPosY = e.offsetY / window.devicePixelRatio
-
-        point.x = canvasPosX
-        point.y = canvasPosY
-
-        const {length, sub} = viewer.utils.vector2D
-
-        circle.radius = length(sub({x: canvasPosX, y: canvasPosY}, { x: 200, y: 200}))
-      })
-      }
-
-    onMounted(() => {
-      addObjects(document.getElementById("canvas"))
-    })
-  },
-}
-</script>
-
-<style lang="scss">
-.home {
-  text-align: center;
-  img, canvas {
+<style>
+.home img, canvas {
     margin: 24px;
-  }
-  canvas {
-    width: 400px;
+
+}
+
+.home canvas {
+      width: 400px;
     height: 400px;
-  }
 }
 </style>
--- docs/README.md
+++ docs/README.md
@@ -3,7 +3,7 @@ sidebar: false
 next: /guide/index.html
 ---
 
-<Home />
+<Home/>
 
 ::: tip
 To learn how to use the 2D Engine **step by step**, visite [the guide page](/guide/index.html).
